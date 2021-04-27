8efdcd8a97c32f170b151e2f1ff2970bc341a27e
philippschalau <philipp.schalau@hotmail.de>
2021-04-27 18:37:07 +0200

    Hello World SPring App

---
+++ src/main/java/htw/berlin/getvac/GetvacApplication.java
@@ -0,0 +1,13 @@
+package htw.berlin.getvac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetvacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetvacApplication.class, args);
+    }
+
+}
