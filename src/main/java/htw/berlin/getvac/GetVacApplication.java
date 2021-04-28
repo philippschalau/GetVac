3bfa53091a0779402001d0af8c03f2b41e42d810
philippschalau <philipp.schalau@hotmail.de>
2021-04-28 19:40:02 +0200

    klassennamen geändert

--- src/main/java/htw/berlin/getvac/GetVacApplication.java
+++ src/main/java/htw/berlin/getvac/GetVacApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GetvacApplication {
+public class GetVacApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GetvacApplication.class, args);
+        SpringApplication.run(GetVacApplication.class, args);
     }
 
 }
