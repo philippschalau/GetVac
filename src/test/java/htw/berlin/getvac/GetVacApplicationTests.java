d9419216154db58ba85d92ac57919b78096efa89
philippschalau <philipp.schalau@hotmail.de>
2021-04-29 09:58:04 +0200

    bug gefixed mit hilfe von https://www.javaer101.com/en/article/54034390.html

--- src/test/java/htw/berlin/getvac/GetVacApplicationTests.java
+++ src/test/java/htw/berlin/getvac/GetVacApplicationTests.java
@@ -3,7 +3,7 @@ package htw.berlin.getvac;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GetVacApplicationTests.class)
 class GetVacApplicationTests {
 
     @Test
