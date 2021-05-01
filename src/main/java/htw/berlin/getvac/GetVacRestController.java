403f41c67f34ab1670ec55e062b7ff505b5d14cc
philippschalau <philipp.schalau@hotmail.de>
2021-05-01 14:58:07 +0200

    erste REST Versuche

--- src/main/java/htw/berlin/getvac/GetVacRestController.java
+++ src/main/java/htw/berlin/getvac/GetVacRestController.java
@@ -2,28 +2,38 @@ package htw.berlin.getvac;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class GetVacRestController {
+public class GetVacRestController<json> {
 
-    @Autowired
-    Environment env;
-    //private TestCenter testCenter;
+private final TestCenterRepository testCenterRepository;
 
-//    @GetMapping("/")
-//    public String index() {
-//
-//        return env.getProperty("JDBC_DATABASE_URL");
-//    }
+GetVacRestController(TestCenterRepository testCenterRepository){
+    this.testCenterRepository = testCenterRepository;
+}
 
     @GetMapping("/")
     public String index() {
 
         return "Hello World!";
     }
+
+    @GetMapping("/centers")
+    List<TestCenter> all(){
+    return testCenterRepository.findAll();
+    }
+
+    @PostMapping("/centers")
+    TestCenter newTestCenter(@RequestBody TestCenter newTestCenter){
+    return testCenterRepository.save(newTestCenter);
+    }
+
 
 
 
--- src/main/java/htw/berlin/getvac/TestCenterRepository.java
+++ src/main/java/htw/berlin/getvac/TestCenterRepository.java
@@ -1,8 +1,8 @@
 package htw.berlin.getvac;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestCenterRepository extends CrudRepository<TestCenter, Long> {
+public interface TestCenterRepository extends JpaRepository<TestCenter, Long> {
 }
