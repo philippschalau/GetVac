6a68032c551780e07fdffc342ccbc6e51ce6af6f
philippschalau <philipp.schalau@hotmail.de>
2021-06-10 12:05:33 +0200

    fixed okta logout

--- src/main/java/htw/berlin/getvac/SecurityConfig.java
+++ src/main/java/htw/berlin/getvac/SecurityConfig.java
@@ -1,14 +1,30 @@
 package htw.berlin.getvac;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+import java.net.URI;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    ClientRegistrationRepository clientRegistrationRepository;
+
+    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri(URI.create("http://localhost:8080/"));
+        return successHandler;
+    }
+
 
     @Override
     public void configure(HttpSecurity http) throws Exception{
@@ -26,6 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .logout().logoutSuccessUrl("/")
+
+                .and().logout().logoutSuccessHandler(oidcLogoutSuccessHandler())
 
                 .and()
                 .oauth2Client()
--- src/test/java/htw/berlin/getvac/GetVacApplicationTests.java
+++ src/test/java/htw/berlin/getvac/GetVacApplicationTests.java
@@ -1,15 +1,13 @@
-/*
-package htw.berlin.getvac;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GetVacApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
-*/
+//package htw.berlin.getvac;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GetVacApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
