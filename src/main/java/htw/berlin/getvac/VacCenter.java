bd53083d7596f36620041eb88a2c649f8f85c39e
philippschalau <philipp.schalau@hotmail.de>
2021-06-23 22:30:16 +0200

    bilder zu bewertungen hinzugefügt

--- src/main/java/htw/berlin/getvac/VacCenter.java
+++ src/main/java/htw/berlin/getvac/VacCenter.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 public class VacCenter {
 
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String street;
