41f5bc0129d546e79542c76001a68ec015014573
philippschalau <philipp.schalau@hotmail.de>
2021-06-27 21:30:19 +0200

    erster versuch frontend test

---
+++ src/main/resources/static/js/myscripts.test.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+import MyScripts from './myscripts.js';
+
+test('should change the variable show from false to true when button is clicked', async () => {
+
+    const wrapper = mount(MyScripts);
+    wrapper.show = false;
+    await wrapper.find('button').trigger('click');
+    expect(wrapper.show).toBe(true);
+})
