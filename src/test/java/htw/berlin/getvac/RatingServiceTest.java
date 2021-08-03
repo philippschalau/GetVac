0528cafdf0e2c5efb9fc07e0cf842925ebad5749
philippschalau <philipp.schalau@hotmail.de>
2021-08-03 23:31:37 +0200

    Karte auf rating.html erneuert

--- src/test/java/htw/berlin/getvac/RatingServiceTest.java
+++ src/test/java/htw/berlin/getvac/RatingServiceTest.java
@@ -7,14 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 public class RatingServiceTest {
